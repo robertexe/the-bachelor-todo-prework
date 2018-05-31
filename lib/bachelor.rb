f25735467945b9cc6159c66ee24414e30f16dc28
Robert Tabares <robtabares92@gmail.com>
2018-05-31 14:28:53 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,56 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |seasons, hash|
+    if seasons.to_s == season
+      data[seasons].each do |keys,values|
+        if keys["status"] == "Winner"
+          winner = keys["name"].split(' ')[0]
+          return winner
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |seasons, hash|
+    hash.each do |k|
+      if k["occupation"] == occupation
+        return k["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0;
+  data.each do |seasons, hash|
+    hash.each do |k|
+      if k["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  return counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |seasons, hash|
+    hash.each do |k|
+      if k["hometown"] == hometown
+        return k["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_array = [];
+  data.each do |seasons, hash|
+    if seasons.to_s == season
+      data[seasons].each do |keys, values|
+        age_array << keys["age"].to_f
+      end
+    end
+  end
+  age_array.sum / age_array.length
 end
