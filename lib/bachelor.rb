5776fb17c916382fca7b190ac4a082240026b961
Robert Tabares <robtabares92@gmail.com>
2018-05-31 14:29:26 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -48,7 +48,7 @@ def get_average_age_for_season(data, season)
   data.each do |seasons, hash|
     if seasons.to_s == season
       data[seasons].each do |keys, values|
-        age_array << keys["age"].to_f
+        age_array << keys["age"].to_i
       end
     end
   end
